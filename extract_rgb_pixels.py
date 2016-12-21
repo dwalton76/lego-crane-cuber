1e9c361c3228ddf5131b9cf4b6aa7b2424df3272
Daniel Walton <dwalton76@gmail.com>
2016-12-20 21:50:52 -0500

    Added extract_rgb_pixels.py test cases

--- extract_rgb_pixels.py
+++ extract_rgb_pixels.py
@@ -2,6 +2,7 @@
 
 from PIL import Image
 from pprint import pformat
+import argparse
 import logging
 import json
 import math
@@ -83,7 +84,7 @@ def compress_2d_array(original):
     return result
 
 
-def extract_rgb_pixels(size):
+def extract_rgb_pixels(size, use_test_data):
     dimensions = "%dx%dx%d" % (size, size, size)
     colors = {}
     center_pixels = get_center_pixel_coordinates(dimensions)
@@ -147,7 +148,11 @@ def extract_rgb_pixels(size):
             my_indexes = square_indexes
 
         log.info("%s my_indexes %s" % (side, pformat(my_indexes)))
-        filename = "/tmp/rubiks-side-%s.png" % side
+
+        if use_test_data:
+            filename = "test-data/%s/rubiks-side-%s.png" % (dimensions, side)
+        else:
+            filename = "/tmp/rubiks-side-%s.png" % side
         im = Image.open(filename)
         pix = im.load()
 
@@ -180,10 +185,24 @@ if __name__ == '__main__':
     logging.addLevelName(logging.ERROR, "\033[91m   %s\033[0m" % logging.getLevelName(logging.ERROR))
     logging.addLevelName(logging.WARNING, "\033[91m %s\033[0m" % logging.getLevelName(logging.WARNING))
 
-    if len(sys.argv) < 2 or not sys.argv[1].isdigit() or int(sys.argv[1]) < 2:
-        print("ERROR: Run via 'extract_rgb_pixels.py 3' where 3 means a 3x3x3 cube, 2 means 2x2x2, etc")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Extract RGB values from rubiks cube images")
+    parser.add_argument('--test', action='store_true', default=False)
+    parser.add_argument('size', type=int, help='"3" for 3x3x3, "2" for 2x2x2, etc')
+    args = parser.parse_args()
 
-    size = int(sys.argv[1])
+    if args.test:
+        output = json.dumps(extract_rgb_pixels(args.size, True))
 
-    print(json.dumps(extract_rgb_pixels(size)))
+        if args.size == 2:
+            if output == """{"1": [194, 181, 4], "2": [195, 189, 3], "3": [207, 203, 8], "4": [201, 196, 6], "5": [36, 130, 68], "6": [37, 118, 62], "7": [38, 128, 67], "8": [35, 124, 68], "9": [189, 60, 5], "10": [168, 49, 9], "11": [185, 58, 3], "12": [188, 51, 7], "13": [20, 66, 177], "14": [9, 52, 154], "15": [6, 61, 177], "16": [8, 51, 163], "17": [155, 6, 2], "18": [131, 6, 4], "19": [150, 7, 1], "20": [147, 6, 0], "21": [185, 204, 244], "22": [191, 210, 250], "23": [207, 222, 253], "24": [18, 23, 61]}""":
+                print("PASS: 2x2x2")
+            else:
+                print("FAIL: 2x2x2")
+
+        elif args.size == 3:
+            if output == """{"1": [115, 115, 5], "2": [121, 124, 0], "3": [127, 127, 5], "4": [128, 127, 1], "5": [132, 130, 0], "6": [121, 123, 0], "7": [134, 137, 0], "8": [135, 135, 1], "9": [141, 135, 0], "10": [12, 110, 123], "11": [13, 101, 115], "12": [9, 86, 96], "13": [13, 109, 123], "14": [9, 105, 117], "15": [7, 96, 104], "16": [13, 108, 114], "17": [9, 98, 106], "18": [12, 101, 107], "19": [162, 61, 15], "20": [154, 54, 5], "21": [129, 42, 12], "22": [161, 61, 12], "23": [161, 60, 14], "24": [146, 44, 6], "25": [166, 60, 12], "26": [150, 48, 10], "27": [156, 52, 13], "28": [6, 54, 200], "29": [5, 53, 189], "30": [5, 42, 156], "31": [6, 54, 200], "32": [6, 47, 191], "33": [4, 44, 176], "34": [4, 52, 188], "35": [3, 47, 180], "36": [8, 45, 186], "37": [115, 5, 14], "38": [108, 4, 1], "39": [94, 3, 0], "40": [115, 5, 6], "41": [114, 6, 6], "42": [104, 4, 2], "43": [121, 2, 6], "44": [105, 5, 3], "45": [112, 4, 4], "46": [180, 200, 235], "47": [190, 211, 240], "48": [204, 219, 250], "49": [199, 218, 250], "50": [208, 221, 253], "51": [197, 214, 244], "52": [205, 226, 255], "53": [212, 227, 250], "54": [210, 225, 246]}""":
+                print("PASS: 3x3x3")
+            else:
+                print("FAIL: 3x3x3")
+    else:
+        print(json.dumps(extract_rgb_pixels(args.size, False)))
