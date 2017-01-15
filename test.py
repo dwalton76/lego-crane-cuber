aed03298609944078a229de75603d366f9e68f50
Daniel Walton <dwalton76@gmail.com>
2017-01-15 14:49:46 -0500

    Added test case 3x3x3-random-03

--- test.py
+++ test.py
@@ -7,6 +7,20 @@ import logging
 import sys
 
 
+def rgb_matches(text1, text2):
+    result = True
+    data1 = json.loads(text1)
+    data2 = json.loads(text2)
+
+    for (square_index, rgb1) in sorted(data1.items()):
+        rgb2 = data2[square_index]
+        if rgb1 != rgb2:
+            resulit = False
+            break
+
+    return result
+
+
 def get_rgb_delta(text1, text2):
     result = []
     data1 = json.loads(text1)
@@ -35,6 +49,7 @@ logging.addLevelName(logging.WARNING, "\033[91m%s\033[0m" % logging.getLevelName
 test_cases = (
     ('3x3x3 random 01',    'test-data/3x3x3-random-01.txt'),
     ('3x3x3 random 02',    'test-data/3x3x3-random-02.txt'),
+    ('3x3x3 random 03',    'test-data/3x3x3-random-03.txt'),
 )
 
 results = []
@@ -45,16 +60,16 @@ for (desc, filename) in test_cases:
     log.info("test_dir: %s" % test_dir)
 
     subprocess.call("cp %s/*.png /tmp/" % test_dir, shell=True)
-    output = subprocess.check_output(['./extract_rgb_pixels.py']).decode('ascii').strip()
+    output = subprocess.check_output(['./extract_rgb_pixels.py']).decode('ascii').splitlines()[0].strip()
 
     with open(filename, 'r') as fh:
         expected_output = fh.readlines()[0].strip()
 
-    if output == expected_output:
-       results.append("\033[92mPASS\033[0m: %s" % desc)
+    if rgb_matches(expected_output, output):
+        results.append("\033[92mPASS\033[0m: %s" % desc)
     else:
-       results.append("\033[91mFAIL\033[0m: %s" % desc)
-       results.append(get_rgb_delta(expected_output, output))
-       results.append(output)
+        results.append("\033[91mFAIL\033[0m: %s" % desc)
+        results.append(get_rgb_delta(expected_output, output))
+        results.append(output)
 
 print('\n'.join(results))
