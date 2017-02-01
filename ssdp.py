221f023caf0f4ef671da378fe266bad7ec1a9639
Daniel Walton <dwalton76@gmail.com>
2017-02-01 21:21:10 +0000

    Moved to lightweight ssdp

--- ssdp.py
+++ ssdp.py
@@ -1,127 +1,60 @@
+#!/usr/bin/env python3
+
+#   Copyright 2014 Dan Krause
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 import socket
-import struct
-import sys
-from httplib import HTTPResponse
-from BaseHTTPServer import BaseHTTPRequestHandler
-from StringIO import StringIO
+import httplib
+import StringIO
 
-import gtk
-import gobject
+class SSDPResponse(object):
+    class _FakeSocket(StringIO.StringIO):
+        def makefile(self, *args, **kw):
+            return self
+    def __init__(self, response):
+        r = httplib.HTTPResponse(self._FakeSocket(response))
+        r.begin()
+        self.location = r.getheader("location")
+        self.usn = r.getheader("usn")
+        self.st = r.getheader("st")
+        self.cache = r.getheader("cache-control").split("=")[1]
+    def __repr__(self):
+        return "<SSDPResponse({location}, {st}, {usn})>".format(**self.__dict__)
 
-LIB_ID = 'my_library'
-MCAST_GRP = '239.255.255.250'
-MCAST_PORT = 1900
-SERVICE_LOCS = {'id1': '127.0.0.1:7766', 'id2': '127.0.0.1:7766'}
-
-DISCOVERY_MSG = ('M-SEARCH * HTTP/1.1\r\n' +
-                 'ST: %(library)s:%(service)s\r\n' +
-                 'MX: 3\r\n' +
-                 'MAN: "ssdp:discover"\r\n' +
-                 'HOST: 239.255.255.250:1900\r\n\r\n')
-
-LOCATION_MSG = ('HTTP/1.1 200 OK\r\n' +
-                'ST: %(library)s:%(service)s\r\n'
-                'USN: %(service)s\r\n'
-                'Location: %(loc)s\r\n'
-                'Cache-Control: max-age=900\r\n\r\n')
-
-
-class Request(BaseHTTPRequestHandler):
-
-    def __init__(self, request_text):
-        self.rfile = StringIO(request_text)
-        self.raw_requestline = self.rfile.readline()
-        self.error_code = self.error_message = None
-        self.parse_request()
-
-    def send_error(self, code, message):
-        self.error_code = code
-        self.error_message = message
-
-
-class Response(HTTPResponse):
-
-    def __init__(self, response_text):
-        self.fp = StringIO(response_text)
-        self.debuglevel = 0
-        self.strict = 0
-        self.msg = None
-        self._method = None
-        self.begin()
-
-
-def interface_addresses(family=socket.AF_INET):
-    for fam, _, _, _, sockaddr in socket.getaddrinfo('', None):
-        if family == fam:
-            yield sockaddr[0]
-
-
-def client(timeout=1, retries=5):
+def discover(service, timeout=5, retries=1, mx=3):
+    group = ("239.255.255.250", 1900)
+    message = "\r\n".join([
+        'M-SEARCH * HTTP/1.1',
+        'HOST: {0}:{1}',
+        'MAN: "ssdp:discover"',
+        'ST: {st}','MX: {mx}','',''])
     socket.setdefaulttimeout(timeout)
-
-    for _ in xrange(retries):
-        for addr in interface_addresses():
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-            sock.bind((addr, 0))
-
-            msg = DISCOVERY_MSG % dict(service='id1', library=LIB_ID)
-            for _ in xrange(2):
-                # sending it more than once will
-                # decrease the probability of a timeout
-                sock.sendto(msg, (MCAST_GRP, MCAST_PORT))
-
+    responses = {}
+    for _ in range(retries):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        sock.sendto(message.format(*group, st=service, mx=mx), group)
+        while True:
             try:
-                data = sock.recv(1024)
+                response = SSDPResponse(sock.recv(1024))
+                responses[response.location] = response
             except socket.timeout:
-                pass
-            else:
-                response = Response(data)
-                print response.getheader('Location')
-                return
-
-
-def server(timeout=5):
-
-    socket.setdefaulttimeout(timeout)
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-    sock.bind(('', MCAST_PORT))
-
-    mreq = struct.pack('4sl', socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-
-    cond = gobject.IO_IN | gobject.IO_HUP
-    gobject.io_add_watch(sock, cond, handle_requests)
-
-    gtk.main()
-
-
-def handle_requests(sock, _):
-
-    data, addr = sock.recvfrom(4096)
-    request = Request(data)
-    if not request.error_code and \
-            request.command == 'M-SEARCH' and \
-            request.path == '*' and \
-            request.headers['ST'].startswith(LIB_ID) and \
-            request.headers['MAN'] == '"ssdp:discover"':
-
-        service = request.headers['ST'].split(':', 2)[1]
-        if service in SERVICE_LOCS:
-            loc = SERVICE_LOCS[service]
-            msg = LOCATION_MSG % dict(service=service, loc=loc, library=LIB_ID)
-            sock.sendto(msg, addr)
-
-    return True
+                break
+    return responses.values()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and 'client' in sys.argv[1]:
-        client()
-    else:
-        server()
+    # Example:
+    ssdp.discover("roku:ecp")
