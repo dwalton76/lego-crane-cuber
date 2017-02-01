c4ab3495d6278c179cc0b4f40d137dcdafa83eb5
Daniel Walton <dwalton76@gmail.com>
2017-02-01 22:02:31 +0000

    ssdp-client cleanup

--- ssdp-client.py
+++ ssdp-client.py
@@ -18,10 +18,13 @@ import socket
 import http.client
 import io
 
+
 class SSDPResponse(object):
+
     class _FakeSocket(io.StringIO):
         def makefile(self, *args, **kw):
             return self
+
     def __init__(self, response):
         r = http.client.HTTPResponse(self._FakeSocket(response))
         r.begin()
@@ -29,32 +32,36 @@ class SSDPResponse(object):
         self.usn = r.getheader("usn")
         self.st = r.getheader("st")
         self.cache = r.getheader("cache-control").split("=")[1]
+
     def __repr__(self):
         return "<SSDPResponse({location}, {st}, {usn})>".format(**self.__dict__)
 
-def discover(service, timeout=5, retries=1, mx=3):
+
+def discover(service, timeout=5, mx=3):
     group = ("239.255.255.250", 1900)
+    socket.setdefaulttimeout(timeout)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
     message = "\r\n".join([
         'M-SEARCH * HTTP/1.1',
-        'HOST: {0}:{1}',
+        'HOST: %s:%s' % (group[0], group[1]),
         'MAN: "ssdp:discover"',
-        'ST: {st}','MX: {mx}','',''])
-    socket.setdefaulttimeout(timeout)
-    responses = {}
-    for _ in range(retries):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-        sock.sendto(message.format(*group, st=service, mx=mx), group)
-        while True:
-            try:
-                response = SSDPResponse(sock.recv(1024))
-                responses[response.location] = response
-            except socket.timeout:
-                break
-    return list(responses.values())
+        'ST: %s' % service,
+        'MX: %s' % mx])
+    sock.sendto(bytes(message, 'UTF-8'), group)
+    response = None
+
+    try:
+        response = SSDPResponse(sock.recv(1024))
+    except socket.timeout:
+        pass
+
+    return response
 
 
 if __name__ == '__main__':
     # Example:
-    discover("roku:ecp")
+    print(discover("roku:ecp"))
